b3313758a89a4b79a85cdaba25ab58fbef9be169
David Herron <david@davidherron.com>
2020-04-14 18:03:21 -0700

    Support overriding Sequelize connection params

--- Chapter10/notes/models/sequlz.mjs
+++ Chapter10/notes/models/sequlz.mjs
@@ -16,8 +16,32 @@ export function dbHandle() {
 
 export async function connectDB() {
     if (typeof sequlz === 'undefined') {
-        const yamltext = fs.readFile(process.env.SEQUELIZE_CONNECT, 'utf8');
-        const params = jsyaml.safeLoad(yamltext, 'utf8');
+        const yamltext = await fs.readFile(process.env.SEQUELIZE_CONNECT, 'utf8');
+        const params = await jsyaml.safeLoad(yamltext, 'utf8');
+        if (typeof process.env.SEQUELIZE_DBNAME !== 'undefined'
+                && process.env.SEQUELIZE_DBNAME !== '') {
+            params.dbname = process.env.SEQUELIZE_DBNAME;
+        }
+        if (typeof process.env.SEQUELIZE_DBUSER !== 'undefined'
+                && process.env.SEQUELIZE_DBUSER !== '') {
+            params.username = process.env.SEQUELIZE_DBUSER;
+        }
+        if (typeof process.env.SEQUELIZE_DBPASSWD !== 'undefined'
+                && process.env.SEQUELIZE_DBPASSWD !== '') {
+            params.password = process.env.SEQUELIZE_DBPASSWD;
+        }
+        if (typeof process.env.SEQUELIZE_DBHOST !== 'undefined'
+                && process.env.SEQUELIZE_DBHOST !== '') {
+            params.params.host = process.env.SEQUELIZE_DBHOST;
+        }
+        if (typeof process.env.SEQUELIZE_DBPORT !== 'undefined'
+                && process.env.SEQUELIZE_DBPORT !== '') {
+            params.params.port = process.env.SEQUELIZE_DBPORT;
+        }
+        if (typeof process.env.SEQUELIZE_DBDIALECT !== 'undefined'
+                && process.env.SEQUELIZE_DBDIALECT !== '') {
+            params.params.dialect = process.env.SEQUELIZE_DBDIALECT;
+        }
         debug(`connectDB ${util.inspect(params)}`);
         sequlz = new Sequelize(params.dbname,
                         params.username, params.password,
