452376112a081e19ba2210e4fe604e3e10a2e413
David Herron <david@davidherron.com>
2020-04-20 11:34:54 -0700

    Update to newer example code

--- Chapter02/ls2.js
+++ Chapter02/ls2.js
@@ -1,5 +1,26 @@
 const fs = require('fs').promises;
 
+async function listFiles() {
+  try {
+    var dir = '.';
+    if (process.argv[2]) dir = process.argv[2];
+    const files = await fs.readdir(dir);
+    for (let fn of files) {
+      console.log(fn);
+    }
+  } catch (err) {
+     console.error(err);
+  }
+}
+
+listFiles();
+
+/*
+
+// Some may prefer to use this style
+
+const fs = require('fs').promises;
+
 (async () => {
   var dir = '.';
   if (process.argv[2]) dir = process.argv[2];
@@ -8,3 +29,5 @@ const fs = require('fs').promises;
     console.log(fn);
   }
 })().catch(err => { console.error(err); });
+
+*/
