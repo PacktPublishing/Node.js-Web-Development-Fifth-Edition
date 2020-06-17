9d96abc08092767f35b60656117b5bfc3f3c4683
David Herron <david@davidherron.com>
2020-06-17 13:57:22 -0700

    ECR support scripts

---
+++ Chapter13/ecr/tag.ps1
@@ -0,0 +1,3 @@
+docker tag svc-notes:latest %AWS_USER%.dkr.ecr.%AWS_REGION%.amazonaws.com/svc-notes:latest
+docker tag svc-userauth:latest %AWS_USER%.dkr.ecr.%AWS_REGION%.amazonaws.com/svc-userauth:latest
+
