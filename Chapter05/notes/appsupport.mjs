b78413aa357c35f797c3b745c7ee95dbcab314b3
deepesh-packt <61833008+deepesh-packt@users.noreply.github.com>
2020-07-20 16:58:32 +0530

    Update appsupport.mjs

--- Chapter05/notes/appsupport.mjs
+++ Chapter05/notes/appsupport.mjs
@@ -69,7 +69,6 @@ export function basicErrorHandler(err, req, res, next) {
     // Defer to built-in error handler if headersSent
     // See: http://expressjs.com/en/guide/error-handling.html
     if (res.headersSent) {
-        console.log(`basicErrorHandler HEADERS SENT error ${util.inspect(err)}`);
         return next(err)
     }
     // set locals, only providing error in development
